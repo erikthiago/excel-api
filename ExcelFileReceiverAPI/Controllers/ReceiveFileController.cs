17fe52617df811f47825ddbc1299928dcd8a6a26
Erik Sousa <erik.sousa@quatto.com.br>
2019-05-16 17:10:04 -0300

    Add project files.

---
+++ ExcelFileReceiverAPI/Controllers/ReceiveFileController.cs
@@ -0,0 +1,49 @@
+﻿using ExcelFileReceiverAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ExcelFileReceiverAPI.Controllers
+{
+    [Route("api/receivefile")]
+    [ApiController]
+    public class ReceiveFileController : ControllerBase
+    {
+        Teste teste = new Teste();
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return RedirectToAction("");
+            }
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream).ConfigureAwait(false);
+
+                using (var package = new ExcelPackage(memoryStream))
+                {
+                    for (int i = 1; i <= package.Workbook.Worksheets.Count; i++)
+                    {
+                        var totalRows = package.Workbook.Worksheets[i].Dimension?.Rows;
+                        var totalCollumns = package.Workbook.Worksheets[i].Dimension?.Columns;
+                        for (int j = 1; j <= totalRows.Value; j++)
+                        {
+                            for (int k = 1; k <= totalCollumns.Value; k++)
+                            {
+                                teste.T1 = package.Workbook.Worksheets[i].Cells[j, k].Value.ToString();
+                                teste.T2 = package.Workbook.Worksheets[i].Cells[j, k].Value.ToString();
+                            }
+                        }
+                    }
+
+                    return Content(teste.T1);
+                }
+            }
+        }
+    }
+}
---
+++ ExcelFileReceiverAPI/Models/Teste.cs
@@ -0,0 +1,8 @@
+﻿namespace ExcelFileReceiverAPI.Models
+{
+    public class Teste
+    {
+        public string T1 { get; set; }
+        public string T2 { get; set; }
+    }
+}
